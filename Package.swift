588bec0e069e8decdd634cb1aa9616fb03b12355
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2026-08-12 17:21:23 +0000

    [Update] 0.1.2

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GenesysCloudMobileUserTracking",
-            url: "https://github.com/MyPureCloud/mobile-tracking-ios-sdk/releases/download/v0.1.1/GenesysCloudMobileUserTracking.xcframework.zip",
-            checksum: "5f0b6002ff1426acb0978aba942429c11b2ee98c1a6b53601b600151d3c03cf8"
+            url: "https://github.com/MyPureCloud/mobile-tracking-ios-sdk/releases/download/v0.1.2/GenesysCloudMobileUserTracking.xcframework.zip",
+            checksum: "59b3081eb9cd7933fbde176f4e4c9ac89d32d87bd1bfc6e163cc2e3492841079"
         )
     ]
 )
